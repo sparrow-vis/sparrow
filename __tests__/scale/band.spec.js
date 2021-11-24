a80dce5c2767c761977f16398b87daf052d2cb54
pearmini <subairui@icloud.com>
2021-11-24 11:55:55 +0800

    feat(scale): update band

--- __tests__/scale/band.spec.js
+++ __tests__/scale/band.spec.js
@@ -1,24 +1,16 @@
-import { createBand, bandWidth, bandStep } from '../../src/scale';
+import { createBand } from '../../src/scale';
 
 describe('test Band', () => {
-  const options = {
-    domain: ['a', 'b', 'c'],
-    range: [0, 32],
-    padding: 0.2,
-  };
-
   test('createBand(options) return mapper maps discrete domain to continuous range.', () => {
-    const s = createBand(options);
+    const s = createBand({
+      domain: ['a', 'b', 'c'],
+      range: [0, 32],
+      padding: 0.2,
+    });
     expect(s('a')).toBe(2);
     expect(s('b')).toBe(12);
     expect(s('c')).toBe(22);
-  });
-
-  test('bandWidth(options) returns band width.', () => {
-    expect(bandWidth(options)).toBe(8);
-  });
-
-  test('bandStep(options) returns step width.', () => {
-    expect(bandStep(options)).toBe(10);
+    expect(s.getBandWidth()).toBe(8);
+    expect(s.getStep()).toBe(10);
   });
 });
