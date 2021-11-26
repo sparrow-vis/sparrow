601935266712a3aa4e767601765a913d2b65ec2a
pearmini <subairui@icloud.com>
2021-11-26 20:13:08 +0800

    feat(scale): add constant

--- src/scale/index.js
+++ src/scale/index.js
@@ -2,4 +2,5 @@ export { createLinear, linearNice } from './linear';
 export { createIdentity } from './identity';
 export { createOrdinal } from './ordinal';
 export { createBand } from './band';
+export { createConstant } from './constant';
 export { interpolateNumber } from './utils';
