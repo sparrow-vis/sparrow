71578b93449cfd6e696d11b4bb8e5dc66497917b
pearmini <subairui@icloud.com>
2021-11-28 15:10:28 +0800

    docs: update README

--- __tests__/plot/plot.spec.js
+++ __tests__/plot/plot.spec.js
@@ -4,18 +4,20 @@ import { mount, createDiv } from '../utils';
 describe('plot', () => {
   test('plot with api', () => {
     const data = [
-      { name: 'questions', value: 17 },
-      { name: 'schools', value: 25 },
-      { name: 'philosophers', value: 35 },
+      { genre: 'Sports', sold: 275 },
+      { genre: 'Strategy', sold: 115 },
+      { genre: 'Action', sold: 120 },
+      { genre: 'Shooter', sold: 350 },
+      { genre: 'Other', sold: 150 },
     ];
 
     const chart = sp
       .interval()
       .data(data)
       .encode(
-        sp.x().field('name'),
-        sp.y().field('value'),
-        sp.fill().value('steelblue'),
+        sp.x().field('genre'),
+        sp.y().field('sold'),
+        sp.fill().field('genre'),
       )
       .plot();
 
@@ -24,18 +26,20 @@ describe('plot', () => {
 
   test('plot with object', () => {
     const data = [
-      { name: 'questions', value: 17 },
-      { name: 'schools', value: 25 },
-      { name: 'philosophers', value: 35 },
+      { genre: 'Sports', sold: 275 },
+      { genre: 'Strategy', sold: 115 },
+      { genre: 'Action', sold: 120 },
+      { genre: 'Shooter', sold: 350 },
+      { genre: 'Other', sold: 150 },
     ];
 
     const chart = sp.plot({
       data,
       element: 'interval',
       encode: [
-        { channel: 'x', field: 'name' },
-        { channel: 'y', field: 'value' },
-        { channel: 'fill', value: 'steelblue' },
+        { channel: 'x', field: 'genre' },
+        { channel: 'y', field: 'sold' },
+        { channel: 'fill', field: 'genre' },
       ],
     });
 
