a92943468cacc08ed8ff5d3a53520b1ae082c527
pearmini <subairui@icloud.com>
2021-11-28 14:22:33 +0800

    feat(geometry): add label channel for interval

---
+++ src/utils/vector.js
@@ -0,0 +1,32 @@
+export function equal([x0, y0], [x1, y1]) {
+  return closeTo(x0, x1) && closeTo(y0, y1);
+}
+
+export function closeTo(x, y, tol = 1e-5) {
+  return Math.abs(x - y) < tol;
+}
+
+export function dist([x0, y0], [x1 = 0, y1 = 0] = []) {
+  return Math.sqrt((x0 - x1) ** 2 + (y0 - y1) ** 2);
+}
+
+export function sub([x1, y1], [x0, y0]) {
+  return [x1 - x0, y1 - y0];
+}
+
+export function angleBetween(v0, v1) {
+  const a0 = angle(v0);
+  const a1 = angle(v1);
+  if (a0 * a1 >= 0) return Math.abs(a0 - a1);
+  if (a0 < 0 && a1 > 0) return Math.abs(a0) + a1;
+  return Math.PI * 2 - a0 - Math.abs(a1);
+}
+
+export function angle([x, y]) {
+  const theta = Math.atan2(y, x);
+  return theta;
+}
+
+export function fromDegree(radian) {
+  return (radian * 180) / Math.PI;
+}
