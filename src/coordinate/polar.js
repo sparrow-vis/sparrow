a18d0bb98abb8df8832697ad2e9579a5f48d490e
pearmini <subairui@icloud.com>
2022-02-13 23:23:21 +0800

    feat(coordiante): set default arguments for polar

--- src/coordinate/polar.js
+++ src/coordinate/polar.js
@@ -5,7 +5,11 @@ import { curry } from '../utils/helper';
 
 function coordinate(transformOptions, canvasOptions) {
   const { width, height } = canvasOptions;
-  const { innerRadius, outerRadius, startAngle, endAngle } = transformOptions;
+  const { innerRadius = 0,
+    outerRadius = 1,
+    startAngle = -Math.PI / 2,
+    endAngle = (Math.PI / 2) * 3,
+  } = transformOptions;
   const aspect = width / height;
   const sx = aspect > 1 ? 1 / aspect : 1;
   const sy = aspect > 1 ? 1 : aspect;
