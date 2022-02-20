126a3d69e588e549283f19e528ef2170c6d20272
pearmini <subairui@icloud.com>
2022-02-20 08:34:42 +0800

    feat(statistic): bin, stack, normalize, symmetry

--- __tests__/statistic/stack.spec.js
+++ __tests__/statistic/stack.spec.js
@@ -1,22 +1,22 @@
-import { createStack } from '../../src/statistic';
+import { createStackY } from '../../src/statistic';
 
 describe('stack', () => {
   test('createStack', () => {
-    const data = [
-      { x: 0, y: 1 },
-      { x: 0, y: 2 },
-      { x: 0, y: 3 },
-    ];
-    const stack = createStack({
-      fields: ['y'],
-      as: ['y1', 'y2'],
-      key: (d) => d.x,
+    const data = {
+      index: [0, 1, 2],
+      values: {
+        x: [0, 0, 0],
+        y: [1, 2, 3],
+      },
+    };
+    const stack = createStackY();
+    expect(stack(data)).toEqual({
+      index: [0, 1, 2],
+      values: {
+        x: [0, 0, 0],
+        y1: [0, 1, 3],
+        y: [1, 3, 6],
+      },
     });
-    const stackedData = stack(data);
-    expect(stackedData).toEqual([
-      { x: 0, y: 1, y1: 0, y2: 1 },
-      { x: 0, y: 2, y1: 1, y2: 3 },
-      { x: 0, y: 3, y1: 3, y2: 6 },
-    ]);
   });
 });
--- src/statistic/stack.js
+++ src/statistic/stack.js
@@ -1,27 +1,21 @@
-import { group, identity } from '../utils';
+import { group } from '../utils';
 
-function stack(values, accessor = identity) {
-  const stackedValues = [0];
-  for (const [i, v] of Object.entries(values)) {
-    const pv = stackedValues[i];
-    const cv = pv + accessor(v);
-    stackedValues.push(cv);
-  }
-  return stackedValues;
-}
-
-export function createStack({ fields = [], as = [], key }) {
-  const [y] = fields;
-  const [y1, y2] = as;
-  return (data) => {
-    const series = key ? Array.from(group(data, key).values()) : [data];
-    return series.flatMap((data) => {
-      const stackedValues = stack(data, (d) => d[y]);
-      return data.map((d, i) => ({
-        ...d,
-        [y1]: stackedValues[i],
-        [y2]: stackedValues[i + 1],
-      }));
-    });
+export function createStackY() {
+  return ({ index, values }) => {
+    const { y: Y, x: X } = values;
+    const series = X ? Array.from(group(index, (i) => X[i]).values()) : [index];
+    const newY = new Array(index.length);
+    const newY1 = new Array(index.length);
+    for (const I of series) {
+      for (let py = 0, i = 0; i < I.length; py = newY[I[i]], i += 1) {
+        const index = I[i];
+        newY1[index] = py;
+        newY[index] = py + Y[index];
+      }
+    }
+    return {
+      index,
+      values: { ...values, y: newY, y1: newY1 },
+    };
   };
 }
