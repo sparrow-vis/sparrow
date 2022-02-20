42338b294775b10a4192a5ffbf173b929259b2d3
pearmini <subairui@icloud.com>
2022-02-20 22:34:18 +0800

    test(plot): pass

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,4 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.spec.js'],
   runner: 'jest-electron/runner',
   testEnvironment: 'jest-electron/environment',
-  testPathIgnorePatterns: [
-    '<rootDir>/__tests__/plot/*',
-    // '<rootDir>/__tests__/view/*',
-    // '<rootDir>/__tests__/statistic/*',
-  ],
 };
